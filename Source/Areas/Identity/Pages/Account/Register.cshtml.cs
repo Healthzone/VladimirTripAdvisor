10ad5be9411b9d2d78b95f61f8c39659be1c8bb9
Healthzone <emelyanov.valek@gmail.com>
2022-11-30 00:00:39 +0300

    Реализована форма авторизации и регистрации аккаунтов

--- Source/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Source/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -78,7 +78,7 @@ namespace VladimirTripAdvisor.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "Поле email обязательно для заполнения")]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -87,8 +87,8 @@ namespace VladimirTripAdvisor.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Поле пароль обязательно для заполнения")]
+            [StringLength(100, ErrorMessage = "Пароль должен быть как минимум от {2} и до {1} символов.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -99,11 +99,19 @@ namespace VladimirTripAdvisor.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Пароли не совпадают")]
             public string ConfirmPassword { get; set; }
 
+            [Required(ErrorMessage = "Поле имя обязательно для заполнения")]
+            [Display(Name = "Имя")]
             public string Name{ get; set; }
+
+            [Required(ErrorMessage = "Поле фамилия обязательно для заполнения")]
+            [Display(Name = "Фамилия")]
             public string Surname{ get; set; }
+
+            [Required(ErrorMessage = "Поле номер телефона обязательно для заполнения")]
+            public string PhoneNumber{ get; set; }
         }
 
 
@@ -127,7 +135,7 @@ namespace VladimirTripAdvisor.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 //var user = CreateUser();
-                var user = new UserModel { UserName = Input.Email, Email= Input.Email, Name = Input.Name, Surname = Input.Surname};
+                var user = new UserModel { UserName = Input.Email, Email= Input.Email, Name = Input.Name, Surname = Input.Surname, PhoneNumber = Input.PhoneNumber};
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
