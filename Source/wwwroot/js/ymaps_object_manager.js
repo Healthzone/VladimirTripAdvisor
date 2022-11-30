6780268ff43e894c50110ffad0a29982957725d3
Healthzone <emelyanov.valek@gmail.com>
2022-11-30 21:27:43 +0300

    Импортирована Yandex Maps

---
+++ Source/wwwroot/js/ymaps_object_manager.js
@@ -0,0 +1,34 @@
+﻿ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [56.13, 40.39],
+        zoom: 14
+    }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager)
+    myMap.controls.remove('searchControl');
+    myMap.controls.remove('geolocationControl');
+    myMap.controls.remove('trafficControl');
+    myMap.controls.remove('fullscreenControl');
+    myMap.controls.remove('rulerControl');
+    myMap.controls.remove('typeSelector');
+
+    $.ajax({
+        url: "/ymaps/objects.json"
+    }).done(function (data) {
+        objectManager.add(data);
+    });
+
+}
